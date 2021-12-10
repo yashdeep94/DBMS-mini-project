618a9f18bc67ea4d28338755e0fc9b5102d89393
Yash <yashdevdarekar94@gmail.com>
2021-12-10 12:48:55 +0530

    added recharge channels gui

--- proj_r/urls.py
+++ proj_r/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'proj_r'
+
 urlpatterns = [
     path('', views.recharge, name="Recharge"),
 ]
--- proj_r/views.py
+++ proj_r/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from datetime import date, timedelta
+import json
 
 def recharge(request):
-    return render(request, 'proj_r/recharge.html')
+    if request.method == "POST":
+        user_channels = []
+        for val in request.POST:
+            if val == "csrfmiddlewaretoken" or val == "Inputname" or val == "InputId":
+                continue
+            else:
+                user_channels.append(request.POST.get(val))
+        name = request.POST.get("Inputname")
+        sub_id = request.POST.get("InputId")
+        from_date = date.today()
+        to_date = date.today() + timedelta(30)
+        print(name, sub_id, from_date , to_date, json.dumps(user_channels))
+        # print(user_channels)
+        return render(request, 'proj_r/recharge.html')
+    else:
+        return render(request, 'proj_r/recharge.html')
