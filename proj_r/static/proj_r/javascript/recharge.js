9f3802c7af4a1c09a2c3dca9eb2faadd6e4de53a
Yash <yashdevdarekar94@gmail.com>
2021-12-11 14:31:48 +0530

    added total logic

---
+++ proj_r/static/proj_r/javascript/recharge.js
@@ -0,0 +1,40 @@
+let total = 0;
+let all_checkbox = document.querySelectorAll(".form-check-input");
+
+Array.from(all_checkbox).forEach(function(checkbox){
+    checkbox.addEventListener("click", check);
+})
+
+let rates = {
+    "starPlusHD" : 18, 
+    "zeeTvHD" : 15,
+    "colorsHD" : 20,
+    "&tvHD" : 19,
+    "zeeMarathiHD" : 17,
+    "starPravahHD" : 14,
+    "zeeTalkiesHD" : 15,
+    "colorsMarathiHD" : 17,
+    "starGoldHD" : 19,
+    "setMaxHD" : 20,
+    "zeeCinemaHD" : 15,
+    "&PicturesHD" : 19,
+    "starMoviesHD" : 21,
+    "sonyPixHD" : 22,
+    "moviesNowHD" : 25,
+    "&FlixHD" : 20
+}
+
+function check(e){
+    let element = document.getElementById(e.target.id);
+    let total_block = document.getElementById("total_block");
+    let total_outer = document.getElementById("total_outer");
+    if (element.checked == true){
+        total_outer.style.display = "block";
+        total += rates[element.id];
+        total_block.innerHTML = `${total}`;
+    }
+    else {
+        total -= rates[element.id];
+        total_block.innerHTML = `${total}`;
+    }
+}
